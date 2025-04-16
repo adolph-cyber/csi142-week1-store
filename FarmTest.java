8bbcd378935495ab56443822dd9dfdec1db02f47
adolph-cyber <adolphkatsomolefe@gmail.com>
2025-04-16 14:10:57 +0200

    Create FarmTest.java
    FarmTest for week 1

---
+++ FarmTest.java
@@ -0,0 +1,9 @@
+
+public class FarmTest {
+    public static void main(String[] args) {
+        Product pw = new Product("Tomatoes", "Vegetable", 350, 200, "kg", "Mokgacha farm Pty Ltd.");
+        Product bw = new Product("Potatoes", "Vegetable", 1000, 320, "kg", "Selcious Holdings Pty Ltd.");
+        System.out.println(pw);
+        System.out.println(bw);
+    }
+}
