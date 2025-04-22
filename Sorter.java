2f7899b5a4b6793206ba8cf200d9e36280d21403
Norway-Norad-Gobusamangh <ngobusamangh@gmail.com>
2025-04-22 12:20:02 +0200

    Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -1,13 +1,13 @@
 import java.util.List;
 
-public class Sorter {
+public class Sorter{
 
     // Selection Sort by Price (for Produce)
-    public static void selectionSortByPrice(List<Produce> produceList) {
-        for (int i = 0; i < produceList.size() - 1; i++) {
+    public static void selectionSortByPrice(List<Produce> produceList){
+        for (int i = 0; i < produceList.size() - 1; i++){
             int minIndex = i;
-            for (int j = i + 1; j < produceList.size(); j++) {
-                if (produceList.get(j).getPrice() < produceList.get(minIndex).getPrice()) {
+            for (int j = i + 1; j < produceList.size(); j++){
+                if (produceList.get(j).getPrice() < produceList.get(minIndex).getPrice()){
                     minIndex = j;
                 }
             }
@@ -18,11 +18,11 @@ public class Sorter {
     }
 
     // Insertion Sort by Jersey Number (for Players)
-    public static void insertionSortByJerseyNumber(List<Player> players) {
-        for (int i = 1; i < players.size(); i++) {
+    public static void insertionSortByJerseyNumber(List<Player> players){
+        for (int i = 1; i < players.size(); i++){
             Player key = players.get(i);
             int j = i - 1;
-            while (j >= 0 && players.get(j).getJerseyNumber() > key.getJerseyNumber()) {
+            while (j >= 0 && players.get(j).getJerseyNumber() > key.getJerseyNumber()){
                 players.set(j + 1, players.get(j));
                 j--;
             }
@@ -31,9 +31,9 @@ public class Sorter {
     }
 
     // Linear Search for Produce Name
-    public static int linearSearchProduce(List<Produce> produceList, String name) {
-        for (int i = 0; i < produceList.size(); i++) {
-            if (produceList.get(i).getName().equalsIgnoreCase(name)) {
+    public static int linearSearchProduce(List<Produce> produceList, String name){
+        for (int i = 0; i < produceList.size(); i++){
+            if (produceList.get(i).getName().equalsIgnoreCase(name)){
                 return i;
             }
         }
@@ -41,14 +41,14 @@ public class Sorter {
     }
 
     // Binary Search for Player Jersey Number (requires sorted data)
-    public static int binarySearchPlayer(List<Player> players, int jerseyNumber) {
+    public static int binarySearchPlayer(List<Player> players, int jerseyNumber){
         int left = 0, right = players.size() - 1;
-        while (left <= right) {
+        while (left <= right){
             int mid = left + (right - left) / 2;
-            if (players.get(mid).getJerseyNumber() == jerseyNumber) {
+            if (players.get(mid).getJerseyNumber() == jerseyNumber){
                 return mid;
             }
-            if (players.get(mid).getJerseyNumber() < jerseyNumber) {
+            if (players.get(mid).getJerseyNumber() < jerseyNumber){
                 left = mid + 1;
             } else {
                 right = mid - 1;
