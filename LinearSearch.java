67ab49b65db966edfc28b3e54003f13f4875cfd5
adolph-cyber <adolphkatsomolefe@gmail.com>
2025-04-23 12:15:27 +0200

    Delete LinearSearch.java

--- LinearSearch.java
+++
@@ -1,9 +0,0 @@
-// Linear Search for a string field (e.g., name), returning an index or -1
-    public static int linearSearch(List<String> list, String target) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(target)) {
-                return i;
-            }
-        }
-        return -1;
-    }
