65084a1a0ca56d1afbd9b403fedb650892b82e13
Norway-Norad-Gobusamangh <ngobusamangh@gmail.com>
2025-04-30 11:56:22 +0200

    Update ProduceStore.java

--- ProduceStore.java
+++ ProduceStore.java
@@ -1,4 +1,5 @@
 public class ProduceStore {
+    //Produce Fields
     private String name;
     private String category;
     private double price;
@@ -6,6 +7,7 @@ public class ProduceStore {
     private String unit;
     private String buyer;
 
+    //Constructors to initialize
     public ProduceStore(String name, String category, double price, int quantity, String unit, String buyer)
             throws OperationException {
         if (name == null || name.isEmpty()) throw new OperationException("Product name cannot be empty.");
@@ -23,28 +25,54 @@ public class ProduceStore {
         this.buyer = buyer;
     }
 
-    public String getName() { return name; }
-    public String getCategory() { return category; }
-    public double getPrice() { return price; }
-    public int getQuantity() { return quantity; }
-    public String getUnit() { return unit; }
-    public String getBuyer() { return buyer; }
+    //Getter for Name
+    public String getName() {
+        return name;
+    }
+    
+    //Getter for Catergory
+    public String getCategory() {
+        return category;
+    }
+    
+    //Getter for Price
+    public double getPrice() {
+        return price;
+    }
+    //Getter for Quantity
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    //Getter for Unit
+    public String getUnit() {
+        return unit;
+    }
 
+    //Getter for Buyer
+    public String getBuyer() {
+        return buyer;
+    }
+
+    //Setter for Price
     public void setPrice(double price) throws OperationException {
         if (price < 0) throw new OperationException("Price cannot be negative.");
         this.price = price;
     }
 
+    //Setter for Quantity
     public void setQuantity(int quantity) throws OperationException {
         if (quantity < 0) throw new OperationException("Quantity cannot be negative.");
         this.quantity = quantity;
     }
 
+    //Setter for Unit
     public void setUnit(String unit) throws OperationException {
         if (unit == null || unit.isEmpty()) throw new OperationException("Unit cannot be empty.");
         this.unit = unit;
     }
 
+    //Setter for Buyer
     public void setBuyer(String buyer) throws OperationException {
         if (buyer == null || buyer.isEmpty()) throw new OperationException("Buyer cannot be empty.");
         this.buyer = buyer;
